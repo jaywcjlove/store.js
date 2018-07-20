181ef859e81ab15c24a574a4fec5386830e85647
jaywcjlove <398188662@qq.com>
2018-07-21 03:58:35 +0800

    Change the compilation tools.

--- dist/store.js
+++ dist/store.js
@@ -1,188 +1,177 @@
 /*!
- * storejs v1.0.16
+ * storejs v1.0.17
  * Local storage localstorage package provides a simple API
  * 
- * Copyright (c) 2017 kenny wang <wowohoo@qq.com>
+ * Copyright (c) 2018 kenny wang <wowohoo@qq.com>
  * https://github.com/jaywcjlove/store.js
  * 
  * Licensed under the MIT license.
  */
-(function(f) {
-    if (typeof exports === "object" && typeof module !== "undefined") {
-        module.exports = f();
-    } else if (typeof define === "function" && define.amd) {
-        define([], f);
-    } else {
-        var g;
-        if (typeof window !== "undefined") {
-            g = window;
-        } else if (typeof global !== "undefined") {
-            g = global;
-        } else if (typeof self !== "undefined") {
-            g = self;
-        } else {
-            g = this;
-        }
-        g.store = f();
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.store = factory());
+}(this, (function () { 'use strict';
+
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+    return typeof obj;
+  } : function (obj) {
+    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+  };
+
+  var storage = window.localStorage,
+      even_storage = function even_storage() {};
+
+  function isJSON(obj) {
+    return (typeof obj === "undefined" ? "undefined" : _typeof(obj)) === "object" && Object.prototype.toString.call(obj).toLowerCase() === "[object object]" && !obj.length;
+  }
+  function stringify(val) {
+    return val === undefined || typeof val === "function" ? val + '' : JSON.stringify(val);
+  }
+  function deserialize(value) {
+    if (typeof value !== 'string') {
+      return undefined;
     }
-})(function() {
-    var define, module, exports;
-    if (!window.localStorage) return;
-    var storage = window.localStorage, store, _api, even_storage = function() {};
-    function isJSON(obj) {
-        return typeof obj === "object" && Object.prototype.toString.call(obj).toLowerCase() === "[object object]" && !obj.length;
+    try {
+      return JSON.parse(value);
+    } catch (e) {
+      return value || undefined;
     }
-    function stringify(val) {
-        return val === undefined || typeof val === "function" ? val + "" : JSON.stringify(val);
+  }
+  function isFunction(value) {
+    return {}.toString.call(value) === "[object Function]";
+  }
+  function isArray(value) {
+    return value instanceof Array;
+  }
+  function dealIncognito(storage) {
+    var _KEY = '_Is_Incognit',
+        _VALUE = 'yes';
+    try {
+      storage.setItem(_KEY, _VALUE);
+    } catch (e) {
+      if (e.name === 'QuotaExceededError') {
+        var _nothing = function _nothing() {};
+        storage.__proto__ = { setItem: _nothing, getItem: _nothing, removeItem: _nothing, clear: _nothing };
+      }
+    } finally {
+      if (storage.getItem(_KEY) === _VALUE) storage.removeItem(_KEY);
     }
-    function deserialize(value) {
-        if (typeof value !== "string") {
-            return undefined;
-        }
-        try {
-            return JSON.parse(value);
-        } catch (e) {
-            return value || undefined;
-        }
+    return storage;
+  }
+
+  // deal QuotaExceededError if user use incognito mode in browser
+  storage = dealIncognito(storage);
+
+  function Store() {
+    if (!(this instanceof Store)) {
+      return new Store();
     }
-    function isFunction(value) {
-        return {}.toString.call(value) === "[object Function]";
+  }
+
+  Store.prototype = {
+    set: function set$$1(key, val) {
+      even_storage('set', key, val);
+      if (key && !isJSON(key)) {
+        storage.setItem(key, stringify(val));
+      } else if (key && isJSON(key) && !val) {
+        for (var a in key) {
+          this.set(a, key[a]);
+        }
+      }
+      return this;
+    },
+    get: function get$$1(key) {
+      if (!key) {
+        var ret = {};
+        this.forEach(function (key, val) {
+          return ret[key] = val;
+        });
+        return ret;
+      }
+      if (key.charAt(0) === '?') {
+        return this.has(key.substr(1));
+      }
+      return deserialize(storage.getItem(key));
+    },
+    clear: function clear() {
+      this.forEach(function (key, val) {
+        even_storage('clear', key, val);
+      });
+      storage.clear();
+      return this;
+    },
+    remove: function remove(key) {
+      var val = this.get(key);
+      storage.removeItem(key);
+      even_storage('remove', key, val);
+      return val;
+    },
+    has: function has(key) {
+      return {}.hasOwnProperty.call(this.get(), key);
+    },
+    keys: function keys() {
+      var d = [];
+      this.forEach(function (k) {
+        d.push(k);
+      });
+      return d;
+    },
+    size: function size() {
+      return this.keys().length;
+    },
+    forEach: function forEach(callback) {
+      for (var i = 0; i < storage.length; i++) {
+        var key = storage.key(i);
+        if (callback(key, this.get(key)) === false) break;
+      }
+      return this;
+    },
+    search: function search(str) {
+      var arr = this.keys(),
+          dt = {};
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(str) > -1) dt[arr[i]] = this.get(arr[i]);
+      }
+      return dt;
+    },
+    onStorage: function onStorage(cb) {
+      if (cb && isFunction(cb)) even_storage = cb;
+      return this;
     }
-    function isArray(value) {
-        return value instanceof Array;
+  };
+
+  function store(key, data) {
+    var argm = arguments,
+        _Store = Store(),
+        dt = null;
+    if (argm.length === 0) return _Store.get();
+    if (argm.length === 1) {
+      if (typeof key === "string") return _Store.get(key);
+      if (isJSON(key)) return _Store.set(key);
     }
-    function dealIncognito(storage) {
-        var _KEY = "_Is_Incognit", _VALUE = "yes";
-        try {
-            storage.setItem(_KEY, _VALUE);
-        } catch (e) {
-            if (e.name === "QuotaExceededError") {
-                var _nothing = function() {};
-                storage.__proto__ = {
-                    setItem: _nothing,
-                    getItem: _nothing,
-                    removeItem: _nothing,
-                    clear: _nothing
-                };
-            }
-        } finally {
-            if (storage.getItem(_KEY) === _VALUE) storage.removeItem(_KEY);
-        }
-        return storage;
+    if (argm.length === 2 && typeof key === "string") {
+      if (!data) return _Store.remove(key);
+      if (data && typeof data === "string") return _Store.set(key, data);
+      if (data && isFunction(data)) {
+        dt = null;
+        dt = data(key, _Store.get(key));
+        return dt ? store.set(key, dt) : store;
+      }
     }
-    // deal QuotaExceededError if user use incognito mode in browser
-    storage = dealIncognito(storage);
-    function Store() {
-        if (!(this instanceof Store)) {
-            return new Store();
-        }
+    if (argm.length === 2 && isArray(key) && isFunction(data)) {
+      for (var i = 0; i < key.length; i++) {
+        dt = data(key[i], _Store.get(key[i]));
+        store.set(key[i], dt);
+      }
+      return store;
     }
-    // function dealIncognito (storage) {
-    //   var _KEY = '_Is_Incognit', _VALUE = 'yes';
-    //   try { storage.setItem(_KEY, _VALUE) }
-    //   catch (e) {
-    //     if (e.name === 'QuotaExceededError') {
-    //       var _nothing = function () {};
-    //       storage.__proto__ = { setItem: _nothing, getItem: _nothing, removeItem: _nothing, clear: _nothing };
-    //     }
-    //   }
-    //   finally { if (storage.getItem(_KEY) === _VALUE) storage.removeItem(_KEY); }
-    //   return storage;
-    // }
-    // // deal QuotaExceededError if user use incognito mode in browser
-    // storage = dealIncognito(storage);
-    Store.prototype = {
-        set: function(key, val) {
-            even_storage("set", key, val);
-            if (key && !isJSON(key)) {
-                storage.setItem(key, stringify(val));
-            } else if (key && isJSON(key) && !val) {
-                for (var a in key) this.set(a, key[a]);
-            }
-            return this;
-        },
-        get: function(key) {
-            if (!key) {
-                var ret = {};
-                this.forEach(function(key, val) {
-                    ret[key] = val;
-                });
-                return ret;
-            }
-            if (key.charAt(0) === "?") {
-                return this.has(key.substr(1));
-            }
-            return deserialize(storage.getItem(key));
-        },
-        clear: function() {
-            this.forEach(function(key, val) {
-                even_storage("clear", key, val);
-            });
-            storage.clear();
-            return this;
-        },
-        remove: function(key) {
-            var val = this.get(key);
-            storage.removeItem(key);
-            even_storage("remove", key, val);
-            return val;
-        },
-        has: function(key) {
-            return {}.hasOwnProperty.call(this.get(), key);
-        },
-        keys: function() {
-            var d = [];
-            this.forEach(function(k, list) {
-                d.push(k);
-            });
-            return d;
-        },
-        size: function() {
-            return this.keys().length;
-        },
-        forEach: function(callback) {
-            for (var i = 0; i < storage.length; i++) {
-                var key = storage.key(i);
-                if (callback(key, this.get(key)) === false) break;
-            }
-            return this;
-        },
-        search: function(str) {
-            var arr = this.keys(), dt = {};
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i].indexOf(str) > -1) dt[arr[i]] = this.get(arr[i]);
-            }
-            return dt;
-        },
-        onStorage: function(cb) {
-            if (cb && isFunction(cb)) even_storage = cb;
-            return this;
-        }
-    };
-    store = function(key, data) {
-        var argm = arguments, _Store = Store(), dt = null;
-        if (argm.length === 0) return _Store.get();
-        if (argm.length === 1) {
-            if (typeof key === "string") return _Store.get(key);
-            if (isJSON(key)) return _Store.set(key);
-        }
-        if (argm.length === 2 && typeof key === "string") {
-            if (!data) return _Store.remove(key);
-            if (data && typeof data === "string") return _Store.set(key, data);
-            if (data && isFunction(data)) {
-                dt = null;
-                dt = data(key, _Store.get(key));
-                return dt ? store.set(key, dt) : store;
-            }
-        }
-        if (argm.length === 2 && isArray(key) && isFunction(data)) {
-            for (var i = 0; i < key.length; i++) {
-                dt = data(key[i], _Store.get(key[i]));
-                store.set(key[i], dt);
-            }
-            return store;
-        }
-    };
-    for (var a in Store.prototype) store[a] = Store.prototype[a];
-    return store;
-});
+  }
+  for (var a in Store.prototype) {
+    store[a] = Store.prototype[a];
+  }
+
+  return store;
+
+})));
---
+++ scripts/build.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+const path = require('path');
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const banner = require('bannerjs');
+const zlib = require('zlib');
+// const pkg = require('../package.json');
+const uglify = require('uglify-js');
+require('colors-cli/toxic');
+
+// see below for details on the options
+const inputOptions = {
+  input: 'lib/main.js',
+  plugins: [
+    nodeResolve(), // so Rollup can find `ms`
+    commonjs(), // so Rollup can convert `ms` to an ES module
+    babel({
+      exclude: 'node_modules/**', // 只编译我们的源代码
+    }),
+  ],
+};
+
+async function build() {
+  // create a bundle
+  const bundle = await rollup.rollup(inputOptions);
+
+  const uglifyOption = {
+    compress: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      warnings: false,
+    },
+    output: {
+      ascii_only: true,
+    },
+  };
+
+  const umd = await bundle.generate({
+    format: 'umd',
+    name: 'store',
+    banner: banner.multibanner(),
+  });
+
+  const umdMinified = `${banner.onebanner()}\n${uglify.minify(umd.code, uglifyOption).code}`;
+
+  const es = await bundle.generate({
+    format: 'es',
+    name: 'store',
+    banner: banner.multibanner(),
+  });
+
+  write('dist/store.js', umd.code)
+    .then(() => write('dist/store.min.js', umdMinified, true))
+    .then(() => write('dist/store.esm.js', es.code));
+}
+
+build();
+
+function write(dest, code, zip) {
+  return new Promise((resolve, reject) => {
+    function report(extra) {
+      console.log(`${(path.relative(process.cwd(), dest)).blue_bt} ${getSize(code).green_bt + (extra || '')}`);
+      resolve();
+    }
+    if (!fs.existsSync(path.dirname(dest))) {
+      fs.mkdirSync(path.dirname(dest));
+    }
+    fs.writeFile(dest, code, (err) => {
+      if (err) return reject(err);
+      if (zip) {
+        zlib.gzip(code, (_err, zipped) => {
+          if (_err) return reject(_err);
+          report(`(gzipped: ${getSize(zipped).green_bt})`);
+        });
+      } else {
+        report();
+      }
+    });
+  });
+}
+
+function getSize(code) {
+  return `${(code.length / 1024).toFixed(2)}kb`;
+}
